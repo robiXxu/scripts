2592b7b451a1c554f321c95fc3613c3e1976354b
Schiriac Robert <robixxu@yahoo.com>
2016-01-23 13:02:40 +0200

    Enter All - steamcompanion.com added

--- steam.js
+++ steam.js
@@ -1,42 +1,71 @@
+//https://steamcompanion.com/gifts/steamcompanion.php
 
-// http://www.steamgifts.com/ajax.php
-// POST
+// script:enter
+// giftID:43612
+// token:
+// action:enter_giveaway
 
-// xsrf_token:5eca2ce2668891364b3089f1a539fc4c
-// do:entry_insert
-// code:bYFjD
+(function(){
+var links = document.getElementsByClassName('giveaway-links');
+var formdataArray = [];
 
-// document.getElementsByClassName('giveaway__heading__name');
-
-// document.getElementsByClassName('giveaway__heading__name')[0].href
-
-var formData = {
-	xsrf_token: '',
-	code: '',
-	do:'entry_insert',
+function formData(id) {
+	this.script= 'enter';
+	this.giftID=id;
+	this.token='';
+	this.action= 'enter_giveaway';
 };
+function getAllData(){
+	if(links.length>0){
+		for(var i =0; i<links.length;i++){
+			if(document.getElementsByClassName('giveaway-links')[i].style.opacity=="0.5"){}else{
+				$.get(document.getElementsByClassName('giveaway-links')[i].href,function(data){
+					var giftID = data.substring(data.indexOf('name="giftID"'),data.indexOf('</form>'));
+					giftID = giftID.replace('name="giftID" value="','');
+					giftID = giftID.replace('" />','');
+					if(giftID.trim() == ""){}else{
+						var item = new formData(giftID.trim());
+						formdataArray.push(item);
+						console.log("Gift with ID [" + item.giftID + "] added to list");
+					}
+				});
+			}
+		}
+		setTimeout(function(){
+			if(formdataArray.length>0){
+				createButton();	
+			}
+		},1500);
+	}
+}
+function enterAll(){
+	for(var i =0; i<formdataArray.length;i++){
+		var item = formdataArray[i];
+		$.post('https://steamcompanion.com/gifts/steamcompanion.php' , {
+			script: item.script,
+			giftID: item.giftID,
+			token: item.token,
+			action: item.action,
+		}).done(function( data ) {
+			console.log(item);
+			console.log(data);
+		 });
+		if(i==formdataArray.length-1){
+			document.getElementById('enterall').innerHTML = "Done";
+		}
+	}
+}
+function createButton(){
+	var li = document.createElement("li");
+	var link = document.createElement("a");
 
-$.get(document.getElementsByClassName('giveaway__heading__name')[1].href,function(data){
-	var form = data.substring(data.indexOf('<input type="hidden" name="xsrf_token" value="'),data.indexOf('<div data-do="entry_insert"'));
-	var oldform = form;
-	var xsrf_token =  form.substring(form.indexOf('value="'),form.indexOf('" />'));
-	formData.xsrf_token = xsrf_token.replace('value="',"");
-	var code =  oldform.substring(oldform.indexOf('name="code" value="'));
-	formData.code = code.replace('name="code" value="','').replace('" />','');
-	console.log(formData.xsrf_token);
-	console.log(formData.code);
-});
-
-$.post('http://www.steamgifts.com/ajax.php' , {
-	xsrf_token:formData.xsrf_token, 
-	do:formData.do,
-	code:formData.code
-}).done(function( data ) {
-    console.log(data);
- });
-
-
-
-
-<input type="hidden" name="xsrf_token" value="5eca2ce2668891364b3089f1a539fc4c">
-<input type="hidden" name="code" value="3Ky4Z">
+	link.innerHTML ="Enter All";
+	link.setAttribute("id","enterall");
+	link.addEventListener('click',function(){
+		enterAll();
+	});
+	li.appendChild(link);
+	document.getElementsByClassName('left')[0].appendChild(li);
+}
+getAllData();
+})();
