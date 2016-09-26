9cf1e624814686e76b71018a64c1b0556f6e20a8
Schiriac Robert <robert.schiriac@assist.ro>
2016-09-26 13:54:45 +0300

    linkedin addCon - refresh and scroll

--- linkedin/add.js
+++ linkedin/add.js
@@ -1,16 +1,19 @@
 ====================================================================
-
+(function(){
 var i=0;
 function addCon(){
   if($('.bt-request-buffed')[i]){
+  	window.scrollBy(0,200)
     $('.bt-request-buffed')[i].click();
     i++;
   }else if(!$('.bt-request-buffed')[i] && i!=0){
     i=0;
   }else{
     clearInterval(massAddInterval);
+    location.reload()
   }
 }
 var massAddInterval = setInterval(addCon,2000);
 
+})();
 ====================================================================
