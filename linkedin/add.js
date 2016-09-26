47569d11c512f8a5132592477ed6af822f2e7d28
Schiriac Robert <schiriac.robert@gmail.com>
2016-09-26 19:16:11 +0000

    add.js edited online with Bitbucket

--- linkedin/add.js
+++ linkedin/add.js
@@ -3,14 +3,14 @@
 var i=0;
 function addCon(){
   if($('.bt-request-buffed')[i]){
-  	window.scrollBy(0,200)
+  	window.scrollBy(0,200);
     $('.bt-request-buffed')[i].click();
     i++;
   }else if(!$('.bt-request-buffed')[i] && i!=0){
     i=0;
   }else{
     clearInterval(massAddInterval);
-    location.reload()
+    location.reload();
   }
 }
 var massAddInterval = setInterval(addCon,2000);
