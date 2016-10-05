161c9043eaab31a862f56dcf18038249393e4a8a
Schiriac Robert <schiriac.robert@gmail.com>
2016-10-05 21:10:55 +0300

    Linkedin FrontPage connect & changes

--- linkedin/add.js
+++ linkedin/add.js
@@ -1,6 +1,17 @@
 ====================================================================
+// ==UserScript==
+// @name         linkedin
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.linkedin.com/people/*
+// @grant        none
+// ==/UserScript==
+
 (function(){
 var i=0;
+var minutes=10;
 function addCon(){
   if($('.bt-request-buffed')[i]){
   	window.scrollBy(0,200);
@@ -10,10 +21,40 @@ function addCon(){
     i=0;
   }else{
     clearInterval(massAddInterval);
-    location.reload();
+      setTimeout(function(){
+          location.reload();
+      },minutes*60);
+
   }
 }
 var massAddInterval = setInterval(addCon,2000);
 
 })();
 ====================================================================
+
+
+// ==UserScript==
+// @name         linkedin homepage - connect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.linkedin.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if($('.connect').length>0){
+        var i=0;
+        var connectInterval = setInterval(function(){
+            if($('.connect')[i]){
+                $('.connect')[i].click();
+            }else if($('.connect').length>0){
+                i=0;
+            }else{
+                clearInterval(connectInterval);
+            }
+        },1500);
+    }
+})();
